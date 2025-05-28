75659a23d73f7528cc32d4fdb7bd01b956153663
komeda <rikomatsu0313@gmail.com>
2025-05-29 02:16:29 +0900

    コントローラ・ビューの修正

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -3,8 +3,16 @@ class PredictionsController < ApplicationController
   end
 
   def create
-  end
+    prediction = Prediction.new(
+      gender: params[:gender],
+      birth_date: params[:birth_date],
+      measurement_date: params[:measurement_date],
+      measurement_type: params[:measurement_type],
+      value: params[:value]
+    )
 
-  def result
+    seasonal_data = prediction.calculate
+
+    redirect_to prediction_result_path, flash: { seasonal_data: seasonal_data, current_season: current_season, current_height: params[:value] }
   end
 end
