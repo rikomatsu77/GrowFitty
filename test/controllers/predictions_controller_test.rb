454a990304b8ed7420334e97b385c373b4e1e773
komeda <rikomatsu0313@gmail.com>
2025-06-01 05:24:55 +0900

    テストコントローラ内容

--- test/controllers/predictions_controller_test.rb
+++ test/controllers/predictions_controller_test.rb
@@ -1,18 +1,5 @@
 require "test_helper"
 
 class PredictionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get new_prediction_url
-    assert_response :success
-  end
 
-  test "should get create" do
-    post create_prediction_url
-    assert_response :success
-  end
-
-  test "should get result" do
-    get prediction_result_url
-    assert_response :success
-  end
 end
