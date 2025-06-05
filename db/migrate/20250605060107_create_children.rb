b5a41a64a694e9e3121cde2330dfda1fa28ca565
komeda <rikomatsu0313@gmail.com>
2025-06-05 15:53:04 +0900

    Childモデルの作成

---
+++ db/migrate/20250605060107_create_children.rb
@@ -0,0 +1,12 @@
+class CreateChildren < ActiveRecord::Migration[7.2]
+  def change
+    create_table :children do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.string :gender, null: false
+      t.datetime :birthday
+
+      t.timestamps
+    end
+  end
+end
