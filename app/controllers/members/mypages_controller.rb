d4f3c659fa1a421a52451bdb692e5f62c619390d
komeda <rikomatsu0313@gmail.com>
2025-06-08 09:19:41 +0900

    マイページ用コントローラ作成

---
+++ app/controllers/members/mypages_controller.rb
@@ -0,0 +1,28 @@
+class Members::MypagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    @user = current_user
+    @children = @user.children.includes(:measurements)
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to members_mypage_path, notice: "プロフィールを更新しました。"
+    else
+      flash.now[:alert] = "更新に失敗しました。"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:user_name, :email)
+  end
+end
