8f7353e03bd3e298aaa02833eccf7595e85bb187
komeda <rikomatsu0313@gmail.com>
2025-09-28 14:27:17 +0900

    S3への画像アップロード設定

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ gem "omniauth-rails_csrf_protection"
 
 gem "carrierwave", "2.2.2"
 
+gem "fog-aws"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -1,11 +1,14 @@
 class PostImageUploader < CarrierWave::Uploader::Base
-  storage :file
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     "post_placeholder"
   end
