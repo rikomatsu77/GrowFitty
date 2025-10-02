98ca348da47052a6710863d88a73935981979d2a
komeda <rikomatsu0313@gmail.com>
2025-10-03 03:06:19 +0900

    静的OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: "GrowFitty",
+      title: "子ども服サイズ予測アプリ",
+      reverse: true,
+      charset: "utf-8",
+      separator: " | ",
+      description: "子どもの成長に合わせて最適な服のサイズを予測。ぴったりサイズで無駄のないお買い物をサポートします。",
+      keywords: "GrowFitty,子ども服,サイズ予測,育児,子育て,服選び,成長記録",
+      canonical: "https://growfitty.onrender.com/",
+      og: {
+        site_name: "GrowFitty",
+        title: :title,
+        description: :description,
+        type: "website",
+        url: "https://growfitty.onrender.com/",
+        image: image_url("GrowFitty-ogp.png"),
+        locale: "ja_JP"
+      },
+      twitter: {
+        card: "summary_large_image",
+        site: "@rikopin_61b",
+        image: image_url("GrowFitty-ogp.png")
+      }
+    }
+  end
 end
