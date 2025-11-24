717067ccab823d0488ff1885fb533fbd7c5ae228
komeda <rikomatsu0313@gmail.com>
2025-11-25 07:30:41 +0900

    lint修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [ :google_oauth2 , :line ]
+         :omniauthable, omniauth_providers: [ :google_oauth2 ,:line ]
 
   has_many :children, dependent: :destroy
   has_many :posts, dependent: :destroy
